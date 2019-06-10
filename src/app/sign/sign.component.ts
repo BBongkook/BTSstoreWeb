876d3af95b3534de0513c1188872d5696b831032
lgt8879 <lgt8879@gmail.com>
2019-06-10 17:05:24 +0900

    daum map address

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -9,19 +9,28 @@ import { SignService } from './sign.service';
 })
 export class SignComponent implements OnInit {
   ss: Sign = new Sign();
-  uiPwd2 :string ="";
+  uiPwd2: string = "";
   constructor(private _ss: SignService) { }
 
   ngOnInit() {
   }
-  sign() : void{
-    if(!this.ss.uiId){
+  sign(): void {
+    if (!this.ss.uiName) {
+      alert("이름을 입력해주세요.");
+    } else if (!this.ss.uiId) {
       alert("아이디를 입력해주세요.");
-    }
-    if(!this.ss.uiPwd){
+    } else if (!this.ss.uiPwd) {
       alert("비밀번호를 입력해주세요.");
-    }else(this.ss.uiPwd != this.uiPwd2)
-    alert("뭐꼬이거?");
-    
+    } else if (this.ss.uiPwd != this.uiPwd2) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+    } else if (!this.ss.uiTrans) {
+      alert("성별을 선택해 주세요.");
+    } else if (!this.ss.uiAddr) {
+
+    } else if (!this.ss.uiAddr2) {
+
+    } else if (!this.ss.uiPhone) {
+
+    }
   }
 }
