1d8631887be0373ac7822a518f27dd17d557dab9
lgt8879 <lgt8879@gmail.com>
2019-06-10 14:14:52 +0900

    sign logic test

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sign } from '../vo/sign';
+import { SignService } from './sign.service';
 
 @Component({
   selector: 'app-sign',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign.component.css']
 })
 export class SignComponent implements OnInit {
-
-  constructor() { }
+  ss: Sign = new Sign();
+  uiPwd2 :string ="";
+  constructor(private _ss: SignService) { }
 
   ngOnInit() {
   }
-
+  sign() : void{
+    if(!this.ss.uiId){
+      alert("아이디를 입력해주세요.");
+    }
+    if(!this.ss.uiPwd){
+      alert("비밀번호를 입력해주세요.");
+    }else(this.ss.uiPwd != this.uiPwd2)
+    alert("뭐꼬이거?");
+    
+  }
 }
---
+++ src/app/sign/sign.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
+import { Sign } from '../vo/sign';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignService {
+  private baseUrl: string = 'http://localhost:88/'
+  constructor() { }
+
+  join(si: Sign) {
+    return ajax.post(this.baseUrl + 'signup', si, { 'Content-Type': 'application/json', 'rxjs-custom-header': 'Rxjs' }
+    )
+  };
+}
