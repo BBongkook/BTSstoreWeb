7d957fe25067a3f03433d5f1a2723ad10108ddcd
lgt8879 <lgt8879@gmail.com>
2019-06-11 13:02:58 +0900

    test commit

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -18,7 +18,22 @@ export class SignComponent implements OnInit {
   setDaumAddressApi(data){
     this.ss.uiAddr=data.addr;
     this.ss.uiZipcode=data.zip;
+    
   }
+
+  themeObj = {
+    //bgColor: "", //바탕 배경색
+    searchBgColor: "#0B65C8", //검색창 배경색
+    //contentBgColor: "", //본문 배경색(검색결과,결과없음,첫화면,검색서제스트)
+    //pageBgColor: "", //페이지 배경색
+    //textColor: "", //기본 글자색
+    queryTextColor: "#FFFFFF" //검색창 글자색
+    //postcodeTextColor: "", //우편번호 글자색
+    //emphTextColor: "", //강조 글자색
+    //outlineColor: "", //테두리
+ };
+ 
+  
   ngOnInit() {
   }
   sign():any{
