d82e82a148dd5f7502d3456e8429234a02ec725d
lgt8879 <lgt8879@gmail.com>
2019-06-17 17:53:00 +0900

    jquery

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  changeValue(evt){
+    $(document).ready(function(){
+      $("#naver").load("/usermanagement");
+  });
+
+  }
+  toggle(){
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
 }
