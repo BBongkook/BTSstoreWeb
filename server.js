2e41aefef290d9c68472954d42c083b87be8ef94
BBongkook <plmqaz6153@gmail.com>
2019-07-29 14:55:10 +0900

    port modify

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ server.get('/*',(req,res)=>{
 })
 
 
-server.listen(process.env.PORT || 3000,()=>{
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.listen(1587,()=>{
+    console.log('server stated at 1587 port!');
 })
